8ca9e74529cf8252c6b00df48d6157b2a00accb0
Max Spliethöver <m.spliethoever@gmail.com>
2016-01-29 12:42:21 +0100

    adapt full functionality to new interface (refactoring still needed)

--- timer.js
+++ timer.js
@@ -8,10 +8,14 @@ var timedisplay;
 var dialpad;
 
 // TODO
+// TODO: rename, doens't fit countdown
 var timerStartTimestamp;
 
 // TODO
 var timerPauseTimestamp;
+
+// TODO
+var startPauseButton;
 
 // TODO
 var lapButton;
@@ -22,22 +26,30 @@ var lapContainer;
 // --------------------------------------------------
 // important functions
 
-// zero-fills the *number* to the given *size* (max. 4 leading zeros)
+// zero-fills the *number* to the given *size* (max. 6 leading zeros)
 function zeroPad (number, size) {
-  var s = "00000" + number;
+  var s = "000000" + number;
   return s.substr(s.length - size);
 };
 
 // returns the time as a string in the format hh:MM:ss:mmm
 // UTC time is used to prevent timezone errors
-function getCurrentTimeString (timestamp) {
-  var date = new Date(timestamp);
+function getCurrentTimeString (options) {
+  // TODO (reafactoring, HTML in JS is not nice...)
+  // TODO (refactoring, Object manipulation is expensive;maybe split here again)
+  if (options.timestamp || options.timestamp == 0) {
+    var date = new Date(options.timestamp);
+    options.hours = date.getUTCHours();
+    options.minutes = date.getUTCMinutes();
+    options.seconds = date.getUTCSeconds();
+    options.milliseconds = date.getUTCMilliseconds();
+  };
   var htmlTimeString = '<span class="time-view__timedisplay" id="timedisplay">'
-    + zeroPad(date.getUTCHours(), 2)
-    + ':' + zeroPad(date.getUTCMinutes(), 2)
-    + ':' + zeroPad(date.getUTCSeconds(), 2)
+    + zeroPad(options.hours, 2)
+    + ':' + zeroPad(options.minutes, 2)
+    + ':' + zeroPad(options.seconds, 2)
     + '</span><span class="time-view__milliseconds">'
-    + '.' + zeroPad(date.getUTCMilliseconds(), 3) + '</span>';
+    + '.' + zeroPad(options.milliseconds, 3) + '</span>';
   return htmlTimeString;
 };
 
@@ -58,74 +70,88 @@ function changeTimeDisplayTo (mode) {
 
   switch (mode) {
     case 'stopwatch':
+      // tab modifications
       countdownTab.classList.remove(menuTabActiveClass);
       stopwatchTab.classList.add(menuTabActiveClass);
       countdownTab.setAttribute('onclick', 'changeTimeDisplayTo("countdown");');
       stopwatchTab.removeAttribute('onclick');
+      // dialpad modifications
       dialpad.classList.add('hide');
+      // button modifications
+      startPauseButton.setAttribute('onclick', 'startStopwatch();');
       lapButton.classList.remove('hide');
+      resetTimer();
       break;
     case 'countdown':
+      // tab modifications
       stopwatchTab.classList.remove(menuTabActiveClass);
       countdownTab.classList.add(menuTabActiveClass);
       stopwatchTab.setAttribute('onclick', 'changeTimeDisplayTo("stopwatch");');
       countdownTab.removeAttribute('onclick');
+      // dialpad/lap modifications
       dialpad.classList.remove('hide');
-      lapButton.classList.add('hide');
       lapContainer.classList.add('hide');
+      // button modifications
+      startPauseButton.setAttribute('onclick', 'startCountdown();');
+      lapButton.classList.add('hide');
+      resetTimer();
       break;
     default:
       console.error('No valid mode to switch to...');
   };
 };
 
+// TODO
+function getActiveMode () {
+  if (countdownTab.classList.contains('menu__tab--active')) {
+    return 'countdown';
+  };
+  return 'stopwatch';
+};
+
 // --------------------------------------------------
 // dialpad functionality
 
-// timestring that is display (zeropadded) in the timeview
-var timeDisplayString = "";
+// TODO
+var timeobject = {
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+    milliseconds: '00'
+};
 
-// extends the timeDisplayString by 'number' and sets it
-// as (time-)formatted string as content of the timeview
+// extends the timeobject by 'number' and updates the timestring
 function extendTimestringWith (number) {
-  if (timeDisplayString.charAt(1) == "0" || timeDisplayString == "") {
-    timeDisplayString = zeroPad(parseInt(timeDisplayString + number), 6);
-    // manipulate DOM to represent new timestring
-    timedisplay.innerHTML = timeDisplayString
-      .replace(/(.{2})/g,":$1")
-      .substr(2);
+  // TODO (refactoring)
+  if (timeobject.hours.substr(0, 1) == '0') {
+    timeobject.hours = timeobject.hours.substr(1)
+      + timeobject.minutes.substr(0, 1);
+    timeobject.minutes = timeobject.minutes.substr(1)
+      + timeobject.seconds.substr(0, 1);
+    timeobject.seconds = timeobject.seconds.substr(1) + number;
+    timedisplay.innerHTML = getCurrentTimeString(timeobject);
   };
 };
 
-// takes the timeDisplayString and creates a timestamp out of it by
-// parsing the numbers from it
-function timeifyString () {
-  var timeStringLength = timeDisplayString.length;
-  // TODO why work with an object?
-  var timeobj = {
-    hours: parseInt(timeDisplayString.substr(timeDisplayString.length - 5, 1)),
-    minutes: parseInt(timeDisplayString.substr(timeDisplayString.length - 4, 2)),
-    seconds: parseInt(timeDisplayString.substr(timeDisplayString.length - 2))
-  };
-
-  return (timeobj.seconds * 1000) + (timeobj.minutes * 60 * 1000)
-    + (timeobj.hours * 60 * 60 * 1000);
-};
-
-// deletes the last number from 'timeDisplayString' and updates the
-// time display span element
+// deletes the last number from timeobject and updates the timestring
 function removeLastTypedNumber () {
-  timeDisplayString = zeroPad(parseInt(timeDisplayString.slice(0, -1)), 6);
-  timedisplay.innerHTML = timeDisplayString
-      .replace(/(.{2})/g,":$1")
-      .substr(2);
+  // TODO (refactoring)
+  timeobject.seconds = timeobject.minutes.substr(1)
+    + timeobject.seconds.substr(0, 1);
+  timeobject.minutes = timeobject.hours.substr(1)
+    + timeobject.minutes.substr(0, 1);
+  timeobject.hours = '0' + timeobject.hours.substr(0, 1);
+  timedisplay.innerHTML = getCurrentTimeString(timeobject);
 };
+
+// TODO
+function calcTypedMilliseconds () {
+  return (timeobject.hours * 60 * 60 * 1000) + (timeobject.minutes * 60 * 1000)
+    + (timeobject.seconds * 1000);
+}
 
 // --------------------------------------------------
 // main timer functionality
-
-// TODO
-var startPauseButton;
 
 // timerIntervalId is used to identify and stop the interval that is used
 // to update the time span/string
@@ -135,121 +161,103 @@ var timerIntervalId;
 // TODO
 var lapLog;
 
-// starts/pauses the stopwatch, depending on actual state
-function startPauseTimer () {
+// starts/resume the stopwatch
+function startStopwatch () {
   if (!timerStartTimestamp) {
     timerStartTimestamp = Date.now();
   };
-  // devide whether 'start' or 'pause' timer
-  if (timerIntervalId) {
-    timerPauseTimestamp = Date.now();
-    window.clearInterval(timerIntervalId);
-    startPauseButton.innerHTML = 'Start';
-    timerIntervalId = null;
-  } else {
-    // set everything to pause and log pause time
-    startPauseButton.innerHTML = 'Pause';
-    if (timerPauseTimestamp) {
-      timerStartTimestamp += Date.now() - timerPauseTimestamp;
-      timerPauseTimestamp = null;
-    };
-    timerIntervalId = window.setInterval(function () {
-      timedisplay.innerHTML = getCurrentTimeString(
-        Date.now() - timerStartTimestamp);
-    }, 75);
+  adaptInterface();
+  addPauseTime();
+  timerIntervalId = window.setInterval(function () {
+    timedisplay.innerHTML = getCurrentTimeString({
+      timestamp: Date.now() - timerStartTimestamp
+    });
+  }, 75);
+};
+
+// TODO
+function startCountdown () {
+  if (!timerStartTimestamp) {
+    timerStartTimestamp = Date.now() + calcTypedMilliseconds();
   };
+  adaptInterface();
+  addPauseTime();
+  timerIntervalId = window.setInterval(function () {
+    var countdownTime = timerStartTimestamp - Date.now();
+    if (countdownTime > 0) {
+      timedisplay.innerHTML = getCurrentTimeString({
+        timestamp: countdownTime
+      });
+    } else {
+      // TODO: play fanfare, etc.
+      resetTimer();
+    };
+  }, 75);
 };
 
 // TODO
 function resetTimer () {
-  if (timerStartTimestamp) {
-    window.clearInterval(timerIntervalId);
-    timerIntervalId = null;
-    timerStartTimestamp = null;
-    timerPauseTimestamp = null;
-    timedisplay.innerHTML = '<span class="time-view__timedisplay">'
-      + '00:00:00</span>'
-      + '<span class="time-view__milliseconds">.000</span>';
-    startPauseButton.innerHTML = 'Start';
+  window.clearInterval(timerIntervalId);
+  timerIntervalId = null;
+  timerStartTimestamp = null;
+  timerPauseTimestamp = null;
+  timeobject = {
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+    milliseconds: '00'
+  };
+  timedisplay.innerHTML = getCurrentTimeString({timestamp: 0});
+  startPauseButton.innerHTML = 'Start';
+  if (getActiveMode() == 'countdown') {
+    startPauseButton.setAttribute('onclick', 'startCountdown();');
+    dialpad.classList.remove('hide');
+  } else if (getActiveMode() == 'stopwatch') {
+    startPauseButton.setAttribute('onclick', 'startStopwatch();');
     lapLog.innerHTML = "";
     lapContainer.classList.add('hide');
   };
-}
+};
+
+// TODO
+function adaptInterface () {
+  startPauseButton.innerHTML = 'Pause';
+  startPauseButton.setAttribute('onclick', 'pauseTimer();');
+  if (getActiveMode() == 'countdown') {
+    dialpad.classList.add('hide');
+  };
+};
+
+// TODO
+function pauseTimer () {
+  timerPauseTimestamp = Date.now();
+  window.clearInterval(timerIntervalId);
+  startPauseButton.innerHTML = 'Start';
+  timerIntervalId = null;
+  if (getActiveMode() == 'countdown') {
+    startPauseButton.setAttribute('onclick', 'startCountdown();');
+  } else if (getActiveMode() == 'stopwatch') {
+    startPauseButton.setAttribute('onclick', 'startStopwatch();');
+  };
+};
+
+// TODO
+function addPauseTime () {
+  if (timerPauseTimestamp) {
+    timerStartTimestamp += Date.now() - timerPauseTimestamp;
+    timerPauseTimestamp = null;
+  };
+};
 
 // TODO
 function logCurrentLap () {
   if (timerStartTimestamp && !timerPauseTimestamp) {
     lapContainer.classList.remove('hide');
     var listNode = document.createElement('li');
-    var lapTime = getCurrentTimeString(Date.now() - timerStartTimestamp);
+    var lapTime = getCurrentTimeString({
+      timestamp: Date.now() - timerStartTimestamp
+    });
     listNode.innerHTML = lapTime;
     lapLog.insertBefore(listNode, lapLog.childNodes[0]);
-  };
-};
-
-
-
-
-// --------------------------------------------------
-// countdown
-// span that contains the countdown; will be set at the end of the html document
-var countdownTimeDisplay;
-
-// main stopwatch button; can have value 'start' and 'pause'
-var countdownStartButton;
-
-// unix timestamp when the countdown should be finished
-var countdownFinishTimestamp;
-
-// unix timestamp when countdown pauses
-var countdownPauseTimestamp;
-
-// countdownIntervallId is used to identify and stop the interval that is used
-// to update the time span/string
-// it is set when the interval starts
-var countdownIntervallId;
-
-// starts/pauses the countdown depending on actual state
-function startPauseCountdown () {
-  var countdownTime = document.getElementById('secondsInput').value * 1000
-    + document.getElementById('minutesInput').value * 60 * 1000
-    + document.getElementById('hoursInput').value * 60 * 60 * 1000;
-  if (!countdownFinishTimestamp && countdownTime > 0) {
-    countdownFinishTimestamp = Date.now() + countdownTime;
-  };
-  if (countdownIntervallId) {
-    countdownPauseTimestamp = Date.now();
-    window.clearInterval(countdownIntervallId);
-    countdownStartButton.value = "Start";
-    countdownIntervallId = null;
-  } else if (countdownFinishTimestamp) {
-    countdownStartButton.value = "Pause";
-    // add pause time to finish time if available
-    if (countdownPauseTimestamp) {
-      countdownFinishTimestamp += Date.now() - countdownPauseTimestamp;
-      countdownPauseTimestamp = null;
-    };
-    countdownIntervallId = window.setInterval(function () {
-      var timestamp = countdownFinishTimestamp - Date.now();
-      if (timestamp > 0) {
-        countdownTimeDisplay.innerHTML = getCurrentTimeString(timestamp);
-      } else {
-        resetCountdown();
-      };
-    }, 75);
-  };
-};
-
-// function to reset the countdown to zero so that a new countdown can be
-// started
-// only works when the countdown were already started
-function resetCountdown () {
-  if (countdownFinishTimestamp) {
-    window.clearInterval(countdownIntervallId);
-    countdownIntervallId = null;
-    countdownStartButton.value = "Start";
-    countdownTimeDisplay.innerHTML = "00:00:00:000";
-    countdownFinishTimestamp = null;
-    countdownPauseTimestamp = null;
   };
 };
